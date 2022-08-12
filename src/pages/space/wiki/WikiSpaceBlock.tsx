6f048541467dc69788ee0e12b6aa23a8a4ad7fb5
Micah <micahscopes@gmail.com>
2022-08-12 00:12:07 -0500

    basic drag + drop

--- src/pages/space/wiki/WikiSpaceBlock.tsx
+++ src/pages/space/wiki/WikiSpaceBlock.tsx
@@ -1,6 +1,7 @@
 import './WikiSpaceBlock.scss'
 import { useObjectState } from '@hyper-hyper-space/react';
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
+import { DragIndicator } from '@mui/icons-material';
 import { Block } from '@hyper-hyper-space/wiki-collab';
 
 import Document from '@tiptap/extension-document'
@@ -10,9 +11,14 @@ import Placeholder from '@tiptap/extension-placeholder'
 import { EditorContent, useEditor } from '@tiptap/react'
 import { MutableReference, Resources } from '@hyper-hyper-space/core';
 import { debounce } from 'lodash-es';
+import { Card, Icon } from '@mui/material';
 
-function WikiSpaceBlock(props: { block: Block, resources: Resources, key: any }) {
-    const author = props.block.getAuthor();
+function WikiSpaceBlock(props: { block: Block, resources: Resources }) {
+    const blockState = useObjectState(props.block);
+    const textState = useObjectState(blockState?.getValue()?.contents);
+    textState?.getValue()?.loadAndWatchForChanges()
+
+    const author = blockState?.getValue()?.getAuthor();
     const editable = author === undefined || author.hasKeyPair();
 
     // for now just use one tiptap `Editor` per block...
@@ -21,7 +27,7 @@ function WikiSpaceBlock(props: { block: Block, resources: Resources, key: any })
 
     const updateBlockWithHtml = useRef(debounce(async (blockContents: MutableReference<string>, html: string) => {
         await blockContents.setValue(html)
-        blockContents.setResources(props.resources!);                    
+        blockContents.setResources(props.resources!);
         blockContents.saveQueuedOps();
         console.log('SAVED BLOCK')
     }, 500))
@@ -31,32 +37,42 @@ function WikiSpaceBlock(props: { block: Block, resources: Resources, key: any })
             Document,
             Paragraph,
             Text,
-            Placeholder.configure({placeholder: 'write something...'})
+            Placeholder.configure({ placeholder: 'write something...' })
         ],
         parseOptions: {
             preserveWhitespace: 'full'
-        },    
+        },
         onUpdate: async ({ editor }) => {
-            if (props.block.contents) {
-                updateBlockWithHtml.current(props.block.contents, editor.getHTML())
+            if (textState) {
+                updateBlockWithHtml.current(textState.getValue()!, editor.getHTML())
             }
         },
         editable
     })
 
-    const textState = useObjectState(props.block.contents);
 
     useEffect(() => {
-        const newText = textState?.value?._value;
+        const newText = textState?.getValue()?.getValue();
 
-        if (newText !== undefined && newText !== editor?.getHTML()) {
+        if(!newText) {
+           console.log('newText is empty!') 
+           return
+        }
+
+        if (newText !== editor?.getHTML()) {
             editor?.commands.setContent(newText, false, { preserveWhitespace: 'full' })
         }
-    }, [textState, editor])
+    }, [textState, editor])//, editor, blockState])
 
-    return (
-        <EditorContent editor={editor}/>
-    )
+    const blockContentView = 
+                    <Card className='wiki-block'>
+                        <Icon>
+                            <DragIndicator></DragIndicator>
+                        </Icon>
+                        <EditorContent editor={editor} />
+                    </Card>
+
+    return blockContentView 
 }
 
 export default WikiSpaceBlock;
--- src/pages/space/wiki/WikiSpacePage.tsx
+++ src/pages/space/wiki/WikiSpacePage.tsx
@@ -4,20 +4,60 @@ import WikiSpaceBlock from './WikiSpaceBlock';
 import { Box, IconButton } from '@mui/material';
 import PostAddIcon from '@mui/icons-material/PostAdd';
 import { Resources } from '@hyper-hyper-space/core';
+import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd';
 
 function WikiSpacePage(props: { page: Page, resources: Resources }) {
-    const blocksState = useObjectState(props.page?.blocks);
+    const blocksListState = useObjectState(props.page?.blocks);
 
     // todo: buttons adding/removing/editing blocks
     // todo: implement drag+drop using react-beautiful-dnd
 
-    const blockElements = blocksState?.getValue()?.contents().map(block => 
-        <WikiSpaceBlock key={block.hash()} block={block} resources={props.resources}></WikiSpaceBlock>
+    const onDragEnd = async (result: DropResult) => {
+        const from = result.source.index;
+        let to = result.destination?.index;
+        if (to === undefined) { return }
+
+        console.log('moving block from', from, 'to', to)
+        const block = blocksListState?.getValue()?.valueAt(from);
+        if (block) {
+            await blocksListState?.getValue()?.deleteAt(from);
+            await blocksListState?.getValue()?.insertAt(block, to);
+            await blocksListState?.getValue()?.save()
+        }
+    }
+
+    
+    const blockElements = blocksListState?.getValue()?.contents().map((block, index) => 
+        (
+            <Draggable draggableId={block.hash()} index={index} key={block.hash()}>
+                {(provided) => 
+                    <div
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                    >
+                            <WikiSpaceBlock block={block} resources={props.resources}></WikiSpaceBlock>
+                    </div>
+                }
+            </Draggable>
+        )
     )
 
     return (
         <Box>
-            <div>{ blockElements }</div>
+            <DragDropContext onDragEnd={onDragEnd}>
+                <Droppable droppableId={props.page.getId()!}>
+                    { provided =>
+                        <div
+                            ref={provided.innerRef}
+                            {...provided.droppableProps}
+                        >
+                            {blockElements}
+                            {provided.placeholder}
+                        </div>
+                    }
+                </Droppable>
+            </DragDropContext>
             <IconButton
                 aria-label="append a new block to the page"
                 onClick={props.page?.addBlock.bind(props.page)}
